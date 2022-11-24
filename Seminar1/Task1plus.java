2628510db3832be540ae05fa6101a72f85da4abc
Ksu <kukinaksu@yandx.ru>
2022-11-24 16:45:05 +0300

    Add files whith Seminar1 and Homework1 all solutions of tasks except task 4 and 9

--- Seminar1/Task1plus.java
+++ Seminar1/Task1plus.java
@@ -1,33 +1,32 @@
-// Задание №1+
+package Seminar1;
 
+// Задание №1+
 // В консоли запросить имя пользователя. В зависимости от текущего времени, вывести приветствие вида 
 // "Доброе утро, <Имя>!", если время от 05:00 до 11:59
 // "Добрый день, <Имя>!", если время от 12:00 до 17:59;
 // "Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
 // "Доброй ночи, <Имя>!", если время от 23:00 до 4:59
 
-package Seminar1;
-
 import java.util.Scanner;
 import java.time.LocalTime;
 
 public class Task1plus {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner iScanner = new Scanner(System.in); // запускаем считывание с консоли
         System.out.printf("Введите Ваше имя: ");
-        String Name = in.next();
-        String s1;
-        int Hour = LocalTime.now().getHour();
+        String Name = iScanner.next(); //считанное с консоли кладем в строковую переменную
+        String a; //вводим дополнительную переменную
+        int Hour = LocalTime.now().getHour(); // переменная с привязкой реального времени
         if (Hour == 23 || Hour < 5)
-            s1 = "Доброй ночи";
+            a = "Доброй ночи";
         else if (Hour < 12)
-            s1 = "Доброе утро";
+            a = "Доброе утро";
         else if (Hour < 18)
-            s1 = "Добрый день";
+            a = "Добрый день";
         else
-            s1 = "Добрый вечер";
-        System.out.println(s1 + ", " + Name + "!");
-        in.close();
+            a = "Добрый вечер";
+        System.out.println(a + ", " + Name + "!"); // вывод результата
+        iScanner.close(); // закрываем считывание с консоли!!!
     }
 
 }
